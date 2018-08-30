7415c60b34f1d2597aeff5d6c9af4f58c393d601
Heeryong Kang <drakang4@users.noreply.github.com>
2018-08-30 18:18:07 +0900

    Added event handles on table rows

--- app/components/Table/Table.tsx
+++ app/components/Table/Table.tsx
@@ -7,6 +7,7 @@ import {
   TableHeaderRowRenderer,
   TableHeaderRenderer,
   TableRowRenderer,
+  RowMouseEventHandlerParams,
 } from 'react-virtualized';
 import styled, { withTheme } from '../../styles/styled-components';
 import { ThemeInterface } from '../../styles/theme';
@@ -33,16 +34,19 @@ const Row = styled.div`
   box-sizing: border-box;
 `;
 
-interface IProps {
+interface Props {
   subtitles: Subtitle[];
   theme: ThemeInterface;
+  timeUpdate(time: number): void;
+  selectSubtitle(indexes: number[]): void;
 }
 
-class Table extends React.Component<IProps> {
+class Table extends React.Component<Props> {
   render() {
     const { subtitles, theme } = this.props;
 
     const gridStyles = { backgroundColor: theme.pallete.gray[8] };
+
     return (
       <AutoSizer>
         {({ width, height }) => (
@@ -56,6 +60,8 @@ class Table extends React.Component<IProps> {
             rowGetter={this.rowGetter}
             rowHeight={32}
             rowRenderer={this.rowRenderer}
+            onRowClick={this.handleRowClick}
+            onRowDoubleClick={this.handleRowDoubleClick}
             overscanRowCount={10}
           >
             <Column
@@ -94,30 +100,83 @@ class Table extends React.Component<IProps> {
 
   rowGetter = ({ index }: { index: number }) => this.props.subtitles[index];
 
-  headerRowRenderer: TableHeaderRowRenderer = ({
-    className,
-    columns,
-    style,
-  }) => {
-    return (
-      <HeaderRow className={className} style={style}>
-        {columns}
-      </HeaderRow>
-    );
+  headerRowRenderer: TableHeaderRowRenderer = ({ columns, ...props }) => {
+    return <HeaderRow {...props}>{columns}</HeaderRow>;
   };
 
   headerRenderer: TableHeaderRenderer = ({ dataKey, label }) => (
     <HeaderCell key={dataKey}>{label}</HeaderCell>
   );
 
-  rowRenderer: TableRowRenderer = ({ className, columns, index, style }) => (
-    <Row key={index} className={className} role="row" style={style}>
-      {columns}
-    </Row>
-  );
+  rowRenderer: TableRowRenderer = ({
+    className,
+    style,
+    columns,
+    index,
+    rowData,
+    onRowClick,
+    onRowDoubleClick,
+    onRowMouseOut,
+    onRowMouseOver,
+    onRowRightClick,
+  }) => {
+    const eventHandlers: {
+      onClick?(event: React.MouseEvent): void;
+      onDoubleClick?(event: React.MouseEvent): void;
+      onMouseOut?(event: React.MouseEvent): void;
+      onMouseOver?(event: React.MouseEvent): void;
+      onContextMenu?(event: React.MouseEvent): void;
+    } = {};
+
+    if (onRowClick) {
+      eventHandlers.onClick = event => onRowClick({ event, index, rowData });
+    }
+    if (onRowDoubleClick) {
+      eventHandlers.onDoubleClick = event =>
+        onRowDoubleClick({ event, index, rowData });
+    }
+    if (onRowMouseOut) {
+      eventHandlers.onMouseOut = event =>
+        onRowMouseOut({ event, index, rowData });
+    }
+    if (onRowMouseOver) {
+      eventHandlers.onMouseOver = event =>
+        onRowMouseOver({ event, index, rowData });
+    }
+    if (onRowRightClick) {
+      eventHandlers.onContextMenu = event =>
+        onRowRightClick({ event, index, rowData });
+    }
+
+    return (
+      <Row
+        key={`row-${index}`}
+        role="row"
+        aria-label="row"
+        className={className}
+        style={style}
+        {...eventHandlers}
+      >
+        {columns}
+      </Row>
+    );
+  };
+
   indexRenderer: TableCellRenderer = ({ rowIndex }) => rowIndex + 1;
+
   timeRenderer: TableCellRenderer = ({ cellData }) => formatMs(cellData);
+
   textRenderer: TableCellRenderer = ({ cellData }) => cellData.join(' ');
+
+  handleRowClick = ({ index }: RowMouseEventHandlerParams) => {
+    this.props.selectSubtitle([index]);
+  };
+
+  handleRowDoubleClick = ({ index }: RowMouseEventHandlerParams) => {
+    const { subtitles } = this.props;
+
+    this.props.timeUpdate(subtitles[index].startTime / 1000);
+  };
 }
 
 export default withTheme(Table);
--- app/containers/Table.tsx
+++ app/containers/Table.tsx
@@ -1,9 +1,24 @@
+import { Dispatch, bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { RootState } from '../reducers';
 import Table from '../components/Table';
+import { selectSubtitle } from '../actions/subtitle';
+import { timeUpdate } from '../actions/player';
 
 const mapStateToProps = (state: RootState) => ({
   subtitles: state.subtitle.data,
 });
 
-export default connect(mapStateToProps)(Table);
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      selectSubtitle,
+      timeUpdate,
+    },
+    dispatch,
+  );
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Table);
