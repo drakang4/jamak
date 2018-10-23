d5b389f2744fd0056b9a5760fdeb5a80fd182528
Heeryong Kang <drakang4@users.noreply.github.com>
2018-10-24 02:42:41 +0900

    Migrate to electron-builder + electron-webpack

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,31 +1,6 @@
 module.exports = {
-  entry: 'main/index.ts',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
   module: {
     rules: [
-      {
-        test: /.tsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              babelrc: false,
-              presets: [
-                [
-                  '@babel/preset-env',
-                  { targets: { node: '8' } }, // or whatever your project requires
-                ],
-                '@babel/preset-typescript',
-              ],
-              plugins: [
-                ['@babel/plugin-proposal-class-properties', { loose: true }],
-              ],
-            },
-          },
-        ],
-      },
       {
         test: /\.md$/,
         use: [
