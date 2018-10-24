34cecbf4a6832f746fa9ffb2901f2e239acf7742
Heeryong Kang <drakang4@users.noreply.github.com>
2018-10-25 00:14:21 +0900

    Updated webpack config

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -21,6 +21,7 @@ module.exports = {
               ],
               plugins: [
                 ['@babel/plugin-proposal-class-properties', { loose: true }],
+                '@babel/plugin-proposal-object-rest-spread',
               ],
             },
           },
